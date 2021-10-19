96fbc1312ef0ee6320c29422f3096c72857c573e
Luis Almeida Matos <luis.matos@iteris.com.br>
2021-10-19 15:54:23 -0300

    species entity

---
+++ DragonBallApi/DragonBallApi/Domain/Entity/Species.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DragonBallApi.Domain.Entity
+{
+    public class Species
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+
+        public virtual CharacterSpecies CharacterSpecies { get; set; }
+    }
+}
