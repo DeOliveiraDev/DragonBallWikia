ca7d45957b29f9d13acc1a72d4a031d7a053c2ed
Vinícius Pereira de Oliveira - VOR <vinicius.pereira@iteris.com.br>
2021-10-20 13:41:13 -0300

    updating project

---
+++ DragonBallApi/DragonBallTests/Characters.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+
+namespace DragonBallTests
+{
+    public class Characters
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
