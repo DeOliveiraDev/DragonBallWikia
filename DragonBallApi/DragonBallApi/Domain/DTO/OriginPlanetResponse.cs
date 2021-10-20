498550cd931badf480083a17ce163e6471cb020e
Gilberto Evangelista Junior - GEV <gilberto.evangelista@iteris.com.br>
2021-10-20 11:52:25 -0300

    Putting changes in OriginPlanetResponse

--- DragonBallApi/DragonBallApi/Domain/DTO/OriginPlanetResponse.cs
+++ DragonBallApi/DragonBallApi/Domain/DTO/OriginPlanetResponse.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DragonBallApi.Domain.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,14 @@ namespace DragonBallApi.Domain.DTO
 {
     public class OriginPlanetResponse
     {
+        public OriginPlanetResponse (OriginPlanet originPlanet)
+        {
+            Id = originPlanet.Id;
+            Name = originPlanet.Name;
+            ImageUrl = originPlanet.ImageUrl;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
