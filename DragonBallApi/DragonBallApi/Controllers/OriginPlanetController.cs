b0de43f65b164cf4a412381af6a82c2357f91a4f
Gilberto Evangelista Junior - GEV <gilberto.evangelista@iteris.com.br>
2021-10-21 11:13:00 -0300

    Adding CRUD methods in the OriginPlanetController

--- DragonBallApi/DragonBallApi/Controllers/OriginPlanetController.cs
+++ DragonBallApi/DragonBallApi/Controllers/OriginPlanetController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DragonBallApi.Domain.DTO;
+using DragonBallApi.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,5 +13,80 @@ namespace DragonBallApi.Controllers
     [ApiController]
     public class OriginPlanetController : ControllerBase
     {
-    }
+        private readonly OriginPlanetService _Service;
+
+        public OriginPlanetController(OriginPlanetService service)
+        {
+            _Service = service;
+        }
+
+		[HttpGet]
+		public async Task<IActionResult> Get(int index, int qtd)
+		{
+			var result = await _Service.Search(index, qtd);
+
+			if (result.Success)
+			{
+				return Ok(result.ObjectReturn);
+			}
+			else
+			{
+				return BadRequest(result);
+			}
+		}
+
+
+		[HttpGet("{id}")]
+		public async Task<IActionResult> GetById(int id)
+		{
+			var result = await _Service.SearchById(id);
+
+			if (result.Success)
+			{
+				return Ok(result.ObjectReturn);
+			}
+			else
+			{
+				return NotFound(result);
+			}
+		}
+
+
+		[HttpPost]
+		// FromBody para indicar de o corpo da requisição deve ser mapeado para o modelo
+		public async Task<IActionResult> Post([FromBody] OriginPlanetCreateRequest postModel)
+		{
+			//Validação modelo de entrada
+			if (ModelState.IsValid)
+			{
+				var result = await _Service.Register(postModel);
+				if (!result.Success)
+					return BadRequest(result);
+				else
+					return Ok(result.ObjectReturn);
+			}
+			else
+			{
+				return BadRequest(ModelState);
+			}
+		}
+
+
+		[HttpPut("{id}")]
+		public async Task<IActionResult> Edita(int id, [FromBody] OriginPlanetUpdateRequest putModel)
+		{
+			if (ModelState.IsValid)
+			{
+				var result = await _Service.Update(id, putModel);
+				if (!result.Success)
+					return BadRequest(result);
+				else
+					return Ok(result.ObjectReturn);
+			}
+			else
+			{
+				return BadRequest(ModelState);
+			}
+		}
+	}
 }
