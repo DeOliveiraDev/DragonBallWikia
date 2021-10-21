f0c2a25f5856c1609d3077e4693fe217621e7203
Gilberto Evangelista Junior - GEV <gilberto.evangelista@iteris.com.br>
2021-10-21 10:13:51 -0300

    Add methods in OriginPlanetRepository

--- DragonBallApi/DragonBallApi/DAL/Repository/OriginPlanetRepository.cs
+++ DragonBallApi/DragonBallApi/DAL/Repository/OriginPlanetRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DragonBallApi.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,36 @@ namespace DragonBallApi.DAL.Repository
 {
     public class OriginPlanetRepository
     {
+        private readonly DragonBallContext _dragonBallContex;
+        public OriginPlanetRepository(DragonBallContext dragonBallContext)
+        {
+            _dragonBallContex = dragonBallContext;
+        }
+
+
+        public async Task<OriginPlanet> SearchById(int id)
+        {
+            return await _dragonBallContex.OriginPlanet.FindAsync(id);
+        }
+
+        public async Task<List<OriginPlanet>> Search (int currentPage,  int numberOfPage)
+        {
+            int amtPagePrevious = currentPage * numberOfPage;
+            return await _dragonBallContex.OriginPlanet.Skip(amtPagePrevious).Take(numberOfPage).ToListAsync();
+        } 
+
+        public async Task<OriginPlanet> Register(OriginPlanet newModel)
+        {
+            _dragonBallContex.OriginPlanet.Add(newModel);
+            await _dragonBallContex.SaveChangesAsync();
+            return newModel;
+        }
+
+        public async Task<OriginPlanet> Update(OriginPlanet model)
+        {
+            _dragonBallContex.OriginPlanet.Add(model).State = EntityState.Modified;
+            await _dragonBallContex.SaveChangesAsync();
+            return model;
+        }
     }
 }
