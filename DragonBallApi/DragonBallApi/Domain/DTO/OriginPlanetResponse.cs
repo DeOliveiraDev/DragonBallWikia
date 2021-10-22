9c0e8461fd6626ff647341889cba8a66d092ec83
Gilberto Evangelista Junior - GEV <gilberto.evangelista@iteris.com.br>
2021-10-22 10:35:18 -0300

    Resolved Conflicts

--- DragonBallApi/DragonBallApi/Domain/DTO/OriginPlanetResponse.cs
+++ DragonBallApi/DragonBallApi/Domain/DTO/OriginPlanetResponse.cs
@@ -13,9 +13,17 @@ namespace DragonBallApi.Domain.DTO
             Id = originPlanet.Id;
             Name = originPlanet.Name;
             ImageUrl = originPlanet.ImageUrl;
+
+            if(originPlanet.Characters != null && originPlanet.Characters.Any())
+            {
+                Characters = new List<CharactersResponse>();
+                Characters.AddRange(originPlanet.Characters.Select(x => new CharactersResponse(x)));
+            }
         }
         public int Id { get; set; }
         public string Name { get; set; }
         public string ImageUrl { get; set; }
+
+        public List<CharactersResponse> Characters { get; set; }
     }
 }
